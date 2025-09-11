c6ad90076a154a83f8eaa43e63e9c5674875e4c5
ParkSungGyu1 <steven0507@naver.com>
2025-09-11 09:46:18 +0900

    lv2

---
+++ src/main/java/com/example/sparta/repository/OrderRepository.java
@@ -0,0 +1,12 @@
+package com.example.sparta.repository;
+
+import com.example.sparta.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+		
+    List<Order> findByCreatedAtIsAfter(LocalDateTime from);
+}
---
+++ src/main/java/com/example/sparta/service/ProductService.java
@@ -0,0 +1,32 @@
+package com.example.sparta.service;
+
+import com.example.sparta.entity.Order;
+import com.example.sparta.entity.Product;
+import com.example.sparta.repository.OrderRepository;
+import com.example.sparta.repository.ProductRepository;
+import com.example.sparta.util.ProductServiceSupport;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final OrderRepository orderRepository;
+    private final ProductRepository productRepository;
+
+    // 최근 일주일동안 가장 많이 팔린 상품 탑 쓰리 가져오기.
+    public List<Product> findHit3Products() {
+        // 조회 시작 기간 : 일주일 전
+        LocalDateTime from = LocalDateTime.now().minusDays(7);
+
+        // 최근 일주일 간 발생한 주문
+        List<Order> orderList = orderRepository.findByCreatedAtIsAfter(from);
+
+        // 주문 리스트로부터 탑 쓰리 상품 리스트 추출
+        return ProductServiceSupport.extractFromOrderList(orderList);
+    }
+}
